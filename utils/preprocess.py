cb4918a0461d53c9815db09e749e9e11b74d53b6
Haein Park <haein.park1907@gmail.com>
2024-03-13 18:15:44 -0400

    update: df_to_windowed_df function added

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -20,6 +20,59 @@ class Preprocess:
             This method pops the Date column to be the index of the df.
         """
         self.df['Date'] = self.df['Date'].dt.date
+        
+    
+    def df_to_windowed_df(self, n=3):
+        
+        first_date = self.df.index[n]  # The minimum date from that's n rows away from the start
+        last_date  = self.df.index[-1]  # The last date
+
+        target_date = first_date
+        
+        dates = []
+        X, Y = [], []
+
+        last_time = False
+        while True:
+            df_subset = self.df.loc[:target_date].tail(n+1)
+            
+            if len(df_subset) != n+1:
+                print(f'Error: Window of size {n} is too large for date {target_date}')
+                return
+
+            values = df_subset['Close'].to_numpy()
+            x, y = values[:-1], values[-1]
+
+            dates.append(target_date)
+            X.append(x)
+            Y.append(y)
+
+            next_week = self.df.loc[target_date:target_date+datetime.timedelta(days=7)]
+            next_datetime_str = str(next_week.head(2).tail(1).index.values[0])
+            next_date_str = next_datetime_str.split('T')[0]
+            year_month_day = next_date_str.split('-')
+            year, month, day = year_month_day
+            next_date = datetime.date(day=int(day), month=int(month), year=int(year))
+            
+            if last_time:
+                break
+            
+            target_date = next_date
+
+            if target_date == last_date:
+                last_time = True
+            
+        ret_df = pd.DataFrame({})
+        ret_df['Target Date'] = dates
+        
+        X = np.array(X)
+        for i in range(0, n):
+            X[:, i]
+            ret_df[f'Target-{n-i}'] = X[:, i]
+        
+        ret_df['Target'] = Y
+
+        return ret_df
     
     
     def dataframe_to_X_y(self, df, window_size=5):
